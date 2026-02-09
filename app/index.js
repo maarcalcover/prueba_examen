35c09ecd053581d8d592d2287549fec1b203c4cb
Tu Nombre <you@example.com>
2026-02-09 10:36:00 +0100

    Test auto deploy

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("OK - AUTO deployed (Runner + Ansible)\n  "));
+app.get("/", (req, res) => res.send("OK - AUToO deployed (Runner + Ansible)\n  "));
 
 app.listen(3000, "0.0.0.0", () => {
   console.log("App listening on port 3000");
